aa5ad4b4f5360dcc3349f197a1da22a4d0dd2033
jjrajani <jjrajani@gmail.com>
2016-04-14 12:31:18 -0400

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,24 @@
 require_relative "blackjack.rb"
 
+def runner
+  welcome
+  total = initial_round
+  total += display_card_total(hit?(total))
+  until total > 21
+    total += display_card_total(hit?(total))
+  end
+  end_game
+
+end
+
+
+
+
 runner
+
+
+
+
+
+
+
